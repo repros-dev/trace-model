99192abb2e18d9500e343b4face25de0a00cfa63
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-10-03 12:57:48 -0700

    Split first demo

--- demo/01-minimal-tro/run.sh
+++ demo/01-minimal-tro/run.sh
@@ -2,14 +2,22 @@
 
 # *****************************************************************************
 
-bash_cell DUMP-1 << END_CELL
+bash_cell import_tro_jsonld << END_CELL
 
-# Import minimal TRO and TROS as JSON-LD and export as N-TRIPLES
-
+# Import minimal TRO and TRS as JSON-LD and export as N-TRIPLES
 geist destroy --dataset kb --quiet
 geist create --dataset kb --quiet
-geist import --format jsonld --file minimal-tros.jsonld
+geist import --format jsonld --file minimal-trs.jsonld
 geist import --format jsonld --file minimal-tro.jsonld
+
+END_CELL
+
+
+bash_cell 'export tro ntriples' << END_CELL
+
+# Import minimal TRO and TRS as JSON-LD and export as N-TRIPLES
 geist export --format nt | sort
 
 END_CELL
+
+
