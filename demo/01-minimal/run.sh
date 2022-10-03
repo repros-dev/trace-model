c4b88a3c6068b6dd6fa6169cda10367f2dca9705
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-10-03 16:03:54 -0700

    Add demo with Type-A TRS

---
+++ demo/01-minimal/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# path to data files
+DATA=../data
+
+# *****************************************************************************
+
+bash_cell import_tro_jsonld << END_CELL
+
+# Import TRO and TRS as JSON-LD and export as N-TRIPLES
+geist destroy --dataset kb --quiet
+geist create --dataset kb --quiet
+geist import --format jsonld --file ${DATA}/trs-minimal.jsonld
+geist import --format jsonld --file ${DATA}/tro-minimal.jsonld
+
+END_CELL
+
+
+bash_cell export_tro_ntriples << END_CELL
+
+# Import TRO and TRS as JSON-LD and export as N-TRIPLES
+geist export --format nt | sort
+
+END_CELL
+
+
