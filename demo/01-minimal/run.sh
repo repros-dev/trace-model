64344f01691612a9aa3d17d1fa89695419dbfbf4
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-10-03 23:58:32 -0700

    Add subClassOf and subPropertyOf relationships to vocabulary

--- demo/01-minimal/run.sh
+++ demo/01-minimal/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # paths to data files
-TRACE=../common/trace
-TRS=../common/trs
-TRO=../common/tro
+COMMON=../common
+TRS=${COMMON}/trs
+TRO=${COMMON}/tro
 
 # *****************************************************************************
 
@@ -12,6 +12,7 @@ bash_cell import_tro_jsonld << END_CELL
 # Import TRO and TRS as JSON-LD and export as N-TRIPLES
 geist destroy --dataset kb --quiet
 geist create --dataset kb --infer owl --quiet
+geist import --format jsonld --file ${COMMON}/trace-vocab.jsonld
 geist import --format jsonld --file ${TRS}/trs-01-minimal.jsonld
 geist import --format jsonld --file ${TRO}/tro-01-from-minimal-trs.jsonld
 
@@ -25,16 +26,6 @@ bash_cell export_ntriples << END_CELL
 geist export --format nt | sort | grep trov
 
 END_CELL
-
-# *****************************************************************************
-
-bash_cell import_trace_vocab_jsonld << END_CELL
-
-# Import TRO and TRS as JSON-LD and export as N-TRIPLES
-geist import --format jsonld --file ${TRACE}/trace-vocab.jsonld
-
-END_CELL
-
 
 # *****************************************************************************
 
--- demo/common/trs-queries.sh
+++ demo/common/trs-queries.sh
@@ -1,4 +1,6 @@
 
+# *****************************************************************************
+
 bash_cell query_trs_policies << END_CELL
 
 # What TRS policies are enforced by the TRS?
@@ -20,6 +22,8 @@ __END_QUERY__
 
 END_CELL
 
+# *****************************************************************************
+
 bash_cell query_tro_policies << END_CELL
 
 # What TRO policies are enforced by the TRS?
@@ -40,3 +44,28 @@ geist query --format table << __END_QUERY__
 __END_QUERY__
 
 END_CELL
+
+# *****************************************************************************
+
+bash_cell query_policies << END_CELL
+
+# What policies are enforced by the TRS?
+
+geist query --format table << __END_QUERY__
+
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX trov: <http://trace.org/trov#>
+
+    SELECT DISTINCT ?trsId ?policyName ?policyDescription
+    WHERE {
+        ?trsId      rdf:type                    trov:TraceSystem .
+        ?trsId      trov:enforcesPolicy         ?policyId .
+        ?policyId   trov:hasPolicyName          ?policyName .
+        ?policyId   trov:hasPolicyDescription   ?policyDescription .
+    } ORDER BY ?trsId ?policyName
+
+__END_QUERY__
+
+END_CELL
+
+# *****************************************************************************
