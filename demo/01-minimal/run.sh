d85db260acad63c63b1a80ecb6c9acad65829511
Timothy McPhillips <tmcphillips@absoluteflow.com>
2022-10-03 23:32:34 -0700

    Enable OWL inference in demos

--- demo/01-minimal/run.sh
+++ demo/01-minimal/run.sh
@@ -11,7 +11,7 @@ bash_cell import_tro_jsonld << END_CELL
 
 # Import TRO and TRS as JSON-LD and export as N-TRIPLES
 geist destroy --dataset kb --quiet
-geist create --dataset kb --quiet
+geist create --dataset kb --infer owl --quiet
 geist import --format jsonld --file ${TRS}/trs-01-minimal.jsonld
 geist import --format jsonld --file ${TRO}/tro-01-from-minimal-trs.jsonld
 
@@ -22,7 +22,7 @@ END_CELL
 bash_cell export_ntriples << END_CELL
 
 # Import TRO and TRS as JSON-LD and export as N-TRIPLES
-geist export --format nt | sort
+geist export --format nt | sort | grep trov
 
 END_CELL
 
