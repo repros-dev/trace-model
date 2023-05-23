df8d9a573f7fe6a6bf0779c16734b76a9c8617cf
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-05-23 15:10:51 -0500

    Update pyshacl demo: add arrangement constraints

--- demo/02-explorations/12-validate-jsonld/03-pyshacl/run.sh
+++ demo/02-explorations/12-validate-jsonld/03-pyshacl/run.sh
@@ -301,3 +301,77 @@ END_PYTHON
 END_CELL
 
 # ------------------------------------------------------------------------------
+
+bash_cell 'validate tro declaration with pyshacl (arrangement links to a valid artifact)' << END_CELL
+
+python3 << END_PYTHON
+
+from pyshacl import validate
+
+tro_jsonld = """
+{
+    "@context": [{
+        "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+        "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+        "trov": "https://w3id.org/trace/2023/05/trov#",
+        "@base": "https://github.com/transparency-certified/trace-model/tree/master/demo/01-trov-examples/01-two-artifacts-no-trp/"
+    }],
+    
+    "@graph": [{
+        "trov:hasComposition": [
+        {
+            "@id": "composition/1",
+            "@type": "trov:ArtifactComposition",
+
+            "trov:hasFingerprint":
+            {
+                "@id": "fp1",
+                "@type": "trov:CompositionFingerprint",
+                "trov:sha256": "sha256.fp1"
+            },
+
+            "trov:hasArtifact": [
+            {
+                "@id": "composition/1/artifact/1",
+                "@type": "trov:ResearchArtifact",
+                "trov:sha256": "art.sha256.1"
+            }]
+        }],
+        "trov:hasArrangement": {
+        "@id": "arrangement/1",
+        "@type": "trov:ArtifactArrangement",
+        "trov:hasLocus": [
+            {
+                "@id": "arrangement/1/locus/1",
+                "@type": "trov:ArtifactLocus",
+                "trov:hasArtifact": { "@id": "composition/1/artifact/1" },
+                "trov:hasLocation": "filepath1"
+            },
+            {
+                "@id": "arrangement/1/locus/2",
+                "@type": "trov:ArtifactLocus",
+                "trov:hasArtifact": { "@id": "composition/1/artifact/100" },
+                "trov:hasLocation": "filepath2"
+            }]
+        }
+    }]
+}
+"""
+
+with open("data/tro.schema.ttl") as fin:
+    tro_schema = fin.read()
+
+r = validate(data_graph=tro_jsonld,
+      shacl_graph=tro_schema,
+      data_graph_format="json-ld",
+      inference='rdfs',
+      debug=True)
+conforms, results_graph, results_text = r
+
+print(results_text)
+
+END_PYTHON
+
+END_CELL
+
+# ------------------------------------------------------------------------------
