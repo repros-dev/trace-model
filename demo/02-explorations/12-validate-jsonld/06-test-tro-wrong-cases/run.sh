e29dfaccfdb829d3d2ac9177448920f32fc6cddf
Meng Li <34143965+Meng6@users.noreply.github.com>
2023-06-22 12:08:30 -0500

    Update demo: test-tro-wrong-cases

--- demo/02-explorations/12-validate-jsonld/06-test-tro-wrong-cases/run.sh
+++ demo/02-explorations/12-validate-jsonld/06-test-tro-wrong-cases/run.sh
@@ -5,7 +5,7 @@ TRO_DECLARATION_SCHEMA_PATH="data/tro.schema.ttl"
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'report string with the validate_tro tool' << END_CELL
+bash_cell 'tro validation 1: refer to a nonexistent artifact' << END_CELL
 
 python3 validate_tro -f data/tro1.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro1.png -of png
 
@@ -13,7 +13,7 @@ END_CELL
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'report string with the validate_tro tool' << END_CELL
+bash_cell 'tro validation 2: refer to a nonexistent artifact' << END_CELL
 
 python3 validate_tro -f data/tro2.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro2.png -of png
 
@@ -21,9 +21,34 @@ END_CELL
 
 # ------------------------------------------------------------------------------
 
-bash_cell 'report string with the validate_tro tool' << END_CELL
+bash_cell 'tro validation 3: refer to a nonexistent artifact' << END_CELL
 
 python3 validate_tro -f data/tro3.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro3.png -of png
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell 'tro validation 4: lack of sha256' << END_CELL
+
+python3 validate_tro -f data/tro4.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro4.png -of png
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+bash_cell 'tro validation 5 export as png: multiple sha256' << END_CELL
+
+python3 validate_tro -f data/tro5.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro5.png -of png
+
+END_CELL
+
+# ------------------------------------------------------------------------------
+
+
+bash_cell 'tro validation 5 export as txt: multiple sha256' << END_CELL
+
+python3 validate_tro -f data/tro5.jsonld -s ${TRO_DECLARATION_SCHEMA_PATH} -o products/tro5.txt -of txt
 
 END_CELL
 
